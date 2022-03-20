76402cbdceb3142a90894ce35e8894d7fa40e612
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-20 18:35:02 +0300

    feat:toggle between pages-hide and unhide payment form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-$(Document).ready(function(){
-  
-   
+$(Document).ready(function () {
+	$('#orderNow').click(function () {
+		$('#ordering').show();
+		$('#orderNow').hide();
+	});
+	$('#cancelNow').click(function () {
+	confirm("Are you sure you want leave this page?")
+    $("#ordering").hide();
+    $("#orderNow").show();
+	
+	});
 
-    $(".order").click(function(){
-        alert("Your Order is being processed")
-        prompt("Enter Your Phone Number")
-    });
+	// $(".ordering").click(function(){
+	//     $("#orderNow").toggle()
+	//     $("#form").toggle()
+	// });
 });
