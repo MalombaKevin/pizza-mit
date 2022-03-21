2a62faa0d7888116034007cc2ed92ac2145e1678
MalombaKevin <kevin.malomba@student.moringaschool.com>
2022-03-21 09:00:08 +0300

    feat: order form optimization- adds table for user to see their order details

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(Document).ready(function () {
+	// click action on image: hide and show
 	$('#orderNow').click(function () {
 		$('#ordering').show();
 		$('#orderNow').hide();
@@ -9,16 +10,45 @@ $(Document).ready(function () {
 		$('#orderNow').show();
 	});
 
-	// DOM
+	// business logic
+	function User(first, phone, size, crust, topping, deliver, locate) {
+		this.customerName = first;
+		this.phoneNo = phone;
+		this.sizePizza = size;
+		this.crustPizza = crust;
+		this.toppingPizza = topping;
+		this.deliverPizza = deliver;
+		this.locationDelivery = locate;
+	}
+    
 
 	$('#form').submit(function (event) {
+        event.preventDefault();
 		var userName = $('#fullName').val();
-		var userContact=$('#phoneNumber').val();
-        var pizzaSize= $('#size').val();
-        var toppingsType=$("#form input:checkbox").val();
-        alert(toppingsType );
-        var delivery=$('#delivery').val();
-        var userLocation=$('#location').val();
-		event.preventDefault();
+		var userContact = $('#phoneNumber').val();
+		var pizzaSize = $('#size').val();
+		var pizzaCrust = $('#crust').val();
+		var toppingsType = $('#form input:checkbox').val();
+		var deliveryOption = $('#delivery').val();
+		var userLocation = $('#location').val();
+
+		var newOrder = new User(
+			userName,
+			userContact,
+			pizzaSize,
+			pizzaCrust,
+			toppingsType,
+            deliveryOption,
+			userLocation
+		);
+		$(".nameOfCustomer").append(" " + newOrder.customerName);
+        $(".phoneNoCustomer").append(" " + newOrder.phoneNo);
+        $(".pizzaSizeCustomer").append(newOrder.sizePizza);
+        $(".crustTypeCustomer").append(newOrder.crustPizza);
+        $(".toppingTypeCustomer").append(newOrder.toppingPizza);
+        $(".deliveryCustomer").append(newOrder.deliverPizza);
+        $(".userLocationDeliver").append(newOrder.locationDelivery);
+
+	
 	});
 });
